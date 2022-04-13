4585fea4eb93cc157a55f7de86e93b3173d08658
Marcos Faino <marcos.faino@ifro.edu.br>
2022-04-12 21:35:59 -0400

    atualizado projeto

--- main.js
+++ main.js
@@ -1,37 +1,63 @@
-const API = "http://localhost:8000/api/v1/produtos";
 
-let produtos = null;
-
-const getProdutos = async () =>{
-  const res = await fetch(API);
-  const data = await res.json();
-  return data;
+const getProdutos = async (url) =>{
+  let res = await fetch(url, {
+    headers: new Headers({
+      'Authorization': 'Token 7897a20d61efc7650dd2a0235e8dd2a780ad5733',
+      'Content-Type': 'application/json'
+    }),
+  });
+  let dados = await res.json();
+  return dados;
 }
 
 function listar(resultados){
+  conteudo.innerHTML = "";
   resultados.forEach(prod => {
     linha = document.createElement("tr");
+    coluna0 = document.createElement("td");
+    textocol0 = document.createTextNode(prod.id);
+    coluna0.appendChild(textocol0);
     coluna1 = document.createElement("td");
     textocol1 = document.createTextNode(prod.nome);
     coluna1.appendChild(textocol1);
     coluna2 = document.createElement("td");
     textocol2 = document.createTextNode(prod.preco);
     coluna2.appendChild(textocol2);
+    linha.appendChild(coluna0);
     linha.appendChild(coluna1);
     linha.appendChild(coluna2);
     conteudo.appendChild(linha)
   });
 }
 
+function buscar_listar_produtos(url){
+  sobre.style.opacity = 0.7;
+  sobre.style.zIndex = 100;
+  getProdutos(url)
+  .then((produtos) =>{
+    listar(produtos.results);
+    if(produtos.previous){
+      anterior.onclick = null;
+      anterior.onclick = function(){
+          buscar_listar_produtos(produtos.previous);
+      };
+    }
+    if(produtos.next){
+      posterior.onclick = null;
+      posterior.onclick =function(){
+        buscar_listar_produtos(produtos.next);
+      };
+    }
+    sobre.style.opacity = 0;
+    sobre.style.zIndex = -100;
+  })
+  .catch((erro) => {
+    console.log("Não foi dessa vez!!! ", erro.message);
+  });
+}
+
 
 
 window.addEventListener("load", function(){
-  getProdutos()
-  .then((meuJson) =>{
-    //console.log(meuJson.results);
-    listar(meuJson.results);
-  })
-  .catch((erro)=>{
-    console.error('Opa! não foi dessa vez! ', erro.message);
-  });
-})
+  buscar_listar_produtos("http://localhost:8000/api/v2/produtos");
+});
